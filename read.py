800abdb27cc1eba4abaf5dcb258cdccac946165d
Dillon Huff <dillonhuff@gmail.com>
2018-01-06 15:32:17 -0800

    Possible repeat in data?

--- read.py
+++ read.py
@@ -128,35 +128,12 @@ def fight_date_cmp(a, b):
     return date_cmp(ad, bd)
 
 fights.sort(fight_date_cmp)
-fights = reversed(fights)
-
-# for fight in fights:
-#     print fight.f0, fight.f1, fight.result, ', ', fight.date.month, '/', fight.date.day, '/', fight.date.year
-
-# lg = LossGraph()
-
-# for line in proc_lines:
-#     fighter0 = line[0]
-#     fighter1 = line[1]
-
-#     result = line[2]
-
-#     if result == 'loss':
-#         lg.addFight(fighter1, fighter0)
-#     elif result == 'win':
-#         lg.addFight(fighter0, fighter1)
-
-# print 'Jon Jones beat ', lg.fighter_wins("/fighter/Jon-Jones-27944")
-# print 'Jon Jones lost to ', lg.fighter_losses("/fighter/Jon-Jones-27944")
-
-# for fighter in lg.getFighters():
-
-#     if len(lg.fighter_losses(fighter)) == 0:
-#         print fighter, 'is undefeated with', len(lg.fighter_wins(fighter)), 'wins'
+fights = list(reversed(fights))
 
 print 'Setting up optimization problem'
 s = Solver()
 
+fights = fights[0:3000]
 for fight in fights:
     if (fight.result == 'win') or (fight.result == 'loss'):
         f0_dt = fight.f0 + '-' + fight.date.to_string()
